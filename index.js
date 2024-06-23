c70aacbc376eae2050178dac8d5d079694f116bd
Rashmika <rashmika@atera.io>
2024-06-23 13:25:58 +0530

    server

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-
 const express = require("express");
+
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+// Database Name
+const dbName = 'myProject';
+
+let resResult = ""
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+  
+  // the following code examples can be pasted here...
+  const findResult = await collection.find({}).toArray();
+  console.log('Found documents =>', findResult);
+  resResult = findResult
+  console.log("Connected to ",dbName)
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,5 +40,5 @@ app.listen(PORT, () => {
 });
 
 app.get("/api", (req, res) => {
-    res.json({ message: "NODEJS API PROJECT" , name: "Rashmika"});
+    res.json(resResult);
   });
